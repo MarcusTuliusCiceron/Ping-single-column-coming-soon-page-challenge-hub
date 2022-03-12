902b2d7bf9c82f73cdbe5f74747d3ed8954179b7
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-03-12 18:09:53 +0100

    test

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function validateEmail(email) 
+{
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+document.querySelector('.page__form__button').addEventListener('click', event => {
+    if (validateEmail(document.querySelector('.page__form__input').value)){
+        document.querySelector('.page__form__validation').classList = 'page__form__validation visible valid';
+        document.querySelector('.page__form__validation').textContent = 'Email sent';
+        document.querySelector('.page__form__input').classList.
+
+
+    }
+    else{
+        document.querySelector('.page__form__validation').classList = 'page__form__validation visible invalid';
+        document.querySelector('.page__form__validation').textContent = 'Oops! Please check your email';
+    }
+
+})
+document.querySelector('.page__form__input').addEventListener('change', function(){
+    if (validateEmail(document.querySelector('.page__form__input').value)){
+        document.querySelector('.page__form__validation').classList = 'page__form__validation visible valid';
+        document.querySelector('.page__form__validation').textContent = 'Email sent';
+
+    }
+    else{
+        document.querySelector('.page__form__validation').classList = 'page__form__validation visible invalid';
+        document.querySelector('.page__form__validation').textContent = 'Oops! Please check your email';
+    }
+})
