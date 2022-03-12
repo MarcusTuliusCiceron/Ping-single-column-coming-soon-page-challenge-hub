5c98eb24ca0a09285f8239bbc8ebec2b598f7e3d
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-03-12 19:19:31 +0100

    complete

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ document.querySelector('.page__form__button').addEventListener('click', event =>
     if (validateEmail(document.querySelector('.page__form__input').value)){
         document.querySelector('.page__form__validation').classList = 'page__form__validation visible valid';
         document.querySelector('.page__form__validation').textContent = 'Email sent';
-        document.querySelector('.page__form__input').classList.
+        document.querySelector('.page__form__input').classList.remove('invalid');
 
 
     }
     else{
         document.querySelector('.page__form__validation').classList = 'page__form__validation visible invalid';
         document.querySelector('.page__form__validation').textContent = 'Oops! Please check your email';
+        document.querySelector('.page__form__input').classList.add('invalid');
     }
 
 })
@@ -23,10 +24,12 @@ document.querySelector('.page__form__input').addEventListener('change', function
     if (validateEmail(document.querySelector('.page__form__input').value)){
         document.querySelector('.page__form__validation').classList = 'page__form__validation visible valid';
         document.querySelector('.page__form__validation').textContent = 'Email sent';
+        document.querySelector('.page__form__input').classList.remove('invalid');
 
     }
     else{
         document.querySelector('.page__form__validation').classList = 'page__form__validation visible invalid';
         document.querySelector('.page__form__validation').textContent = 'Oops! Please check your email';
+        document.querySelector('.page__form__input').classList.add('invalid');
     }
 })
